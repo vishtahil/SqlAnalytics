244de4cea1f200dcb2815619e7424af080979fdd
vishal <vishal.tahil@hotmail.com>
2017-03-08 01:11:14 -0600

    work in progress

---
+++ src/SqlAnalytics/Controllers/AnalyticsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SqlAnalytics.Controllers
+{
+    [Route("api/[controller]")]
+    public class AnalyticsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public class TodoItem
+        {
+            public string Name { get; set; }
+            public bool IsComplete { get; set; }
+        }
+        
+        [HttpPost("Analytics")]
+        public IActionResult GetSqlAnalytics([FromBody]string sql)
+        {
+
+            return Ok(new TodoItem()
+            {
+                Name = "vishal",
+                IsComplete = true
+            });
+        }
+    }
+}
